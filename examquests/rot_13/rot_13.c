6651df19e4246123d75b80d309db8654b8463732
makrofaj67 <dr.ridvanakman@gmail.com>
2024-02-29 19:44:28 +0300

    On branch master  Changes to be committed: 	modified:   ft_putstr/ft_putstr.c 	new file:   rot_13/rot_13 	new file:   rot_13/rot_13.c  Untracked files: 	../exams/

---
+++ examquests/rot_13/rot_13.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+
+{
+	char *s1;
+
+	if (argc != 2)
+		write(1, "\n", 1);
+	else
+	{
+		s1 = argv[1];
+
+		while (*s1)
+		{
+			if (*s1 >= 'a' && *s1 <= 'z')
+			{
+				*s1 = 'a' + (*s1 - 'a' + 13) % 26;
+			}
+			else if (*s1 >= 'A' && *s1 <= 'Z')
+			{
+				*s1 = 'A' + (*s1 - 'A' + 13) % 26;
+			}
+			write(1, s1, 1);
+			s1++;
+		}
+		write(1, "\n", 1);
+	}
+}
