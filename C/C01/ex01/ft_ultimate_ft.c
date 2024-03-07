debdb47fa628f28150e833b3404081a49dc268c1
makrofaj67 <dr.ridvanakman@gmail.com>
2024-03-07 21:38:45 +0300

    sdadada

--- C/C01/ex01/ft_ultimate_ft.c
+++ C/C01/ex01/ft_ultimate_ft.c
@@ -1,29 +1,16 @@
 /******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printcombn.c                                    :+:      :+:    :+:   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rakman <rakman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/07 21:06:19 by rakman            #+#    #+#             */
-/*   Updated: 2024/03/07 21:15:36 by rakman           ###   ########.fr       */
+/*   Created: 2024/03/07 21:22:39 by rakman            #+#    #+#             */
+/*   Updated: 2024/03/07 21:22:41 by rakman           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-#include <stdio.h>
-#include <unistd.h>
-
-void ft_putchar(char c) { write(1, &c, 1); }
-
-void ft_print_combn(int n) {
-  int i = 0;
-  while (n > 0) {
-
-    printf("[%d] || ", n);
-    ft_print_combn(n - 1);
-    n--;
-    i++;
-  }
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
 }
-
-int main(void) { ft_print_combn(4); }
