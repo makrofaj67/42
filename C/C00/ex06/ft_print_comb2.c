c3c738f4bc5a6196895ac78dfa2ed131e2c32c08
makrofaj67 <dr.ridvanakman@gmail.com>
2024-03-07 20:12:53 +0300

    printcomb2corrrect

---
+++ C/C00/ex06/ft_print_comb2.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long	n;
+
+	n = nb;
+	if (n < 0)
+	{
+		n = -1 * n;
+		ft_putchar('-');
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	if (n <= 9)
+		ft_putchar(n + 48);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putnbr(i);
+			ft_putchar(' ');
+			ft_putnbr(j);
+			if (i != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+}
