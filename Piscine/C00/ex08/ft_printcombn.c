17e29a11d6e1ad97fe16e2fc26adaf34ea7d503b
makrofaj67 <dr.ridvanakman@gmail.com>
2024-03-17 19:55:19 +0300

    modified:   C00/ex00/ft_putchar.c 	modified:   C00/ex01/ft_print_alphabet.c 	modified:   C00/ex02/ft_print_reverse_alphabet.c 	modified:   C00/ex03/ft_print_numbers.c 	modified:   C00/ex04/ft_is_negative.c 	deleted:    C00/ex05/a.out 	modified:   C00/ex05/ft_print_comb.c 	deleted:    C00/ex06/a.out 	modified:   C00/ex06/ft_print_comb2.c 	modified:   C00/ex07/ft_putnbr.c 	deleted:    C00/ex08/a.out 	deleted:    C00/ex08/ft_printcombn 	modified:   C00/ex08/ft_printcombn.c 	modified:   C01/ex00/ft_ft.c 	modified:   C01/ex01/ft_ultimate_ft.c 	modified:   C01/ex02/ft_swap.c 	modified:   C01/ex03/ft_div_mod.c 	modified:   C01/ex04/ft_ultimate_div_mod.c 	modified:   C01/ex05/ft_putstr.c 	deleted:    C01/ex06/ft_strlen 	deleted:    C01/ex06/ft_strlen.c 	deleted:    C01/ex06/ft_strlen.o 	deleted:    C01/ex07/a.out 	modified:   C01/ex07/ft_rev_int_tab.c 	deleted:    C01/ex08/a.out 	deleted:    C01/ex08/ft_sort_int_tab 	modified:   C01/ex08/ft_sort_int_tab.c 	deleted:    C02/.vscode/settings.json 	deleted:    C02/.vscode/tasks.json 	deleted:    C02/ex00/a.out 	deleted:    C02/ex00/ft_strcpy 	modified:   C02/ex00/ft_strcpy.c 	deleted:    C02/ex01/a.out 	modified:   C02/ex01/ft_strncpy.c 	modified:   C02/ex02/ft_str_is_alpha.c 	modified:   C02/ex03/ft_str_is_numeric.c 	modified:   C02/ex04/ft_str_is_lowercase.c 	modified:   C02/ex05/ft_str_is_uppercase.c 	modified:   C02/ex06/ft_str_is_printable.c 	modified:   C02/ex07/ft_strupcase.c 	modified:   C02/ex08/ft_strlowcase.c 	deleted:    C02/ex09/a.out 	deleted:    C02/ex09/ft_strcapitalize 	modified:   C02/ex09/ft_strcapitalize.c 	deleted:    C02/ex11/a.out 	modified:   C02/ex11/ft_putstr_non_printable.c 	deleted:    C02/ex12/a.out 	modified:   C02/ex12/ft_print_memory.c 	deleted:    C02/stat.c 	modified:   C03/ex00/ft_strcmp.c 	modified:   C03/ex01/ft_strncmp.c 	modified:   C03/ex02/ft_strcat.c 	modified:   C03/ex03/ft_strncat.c 	modified:   C03/ex04/ft_strstr.c 	modified:   C03/ex05/ft_strlcat.c 	modified:   C04/ex00/ft_strlen.c 	modified:   C04/ex01/ft_putstr.c 	deleted:    C04/ex02/a.out 	modified:   C04/ex02/ft_putnbr.c 	deleted:    C04/ex03/a.out 	deleted:    C04/ex03/ft_atoi 	modified:   C04/ex03/ft_atoi.c 	deleted:    C04/ex04/a.out 	deleted:    C04/ex04/ft_putnbr_base 	modified:   C04/ex04/ft_putnbr_base.c 	modified:   C04/ex05/ft_atoi_base.c 	modified:   C05/ex00/ft_iterative_factorial.c 	modified:   C05/ex01/ft_recursive_factorial.c 	modified:   C05/ex02/ft_iterative_power.c 	modified:   C05/ex03/ft_recursive_power.c 	modified:   C05/ex04/ft_fibonacci.c 	modified:   C05/ex08/ft_the_ten_.c 	modified:   C06/ex00/ft_print_program_name.c 	modified:   C06/ex01/ft_print_params.c 	modified:   C06/ex02/ft_rev_params.c 	modified:   C06/ex03/ft_sort_params.c

--- Piscine/C00/ex08/ft_printcombn.c
+++ Piscine/C00/ex08/ft_printcombn.c
@@ -1,53 +1,36 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_printcombn.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rakman <rakman@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rakman <rakman@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 21:06:19 by rakman            #+#    #+#             */
-/*   Updated: 2024/03/07 21:15:36 by rakman           ###   ########.fr       */
+/*   Updated: 2024/03/17 19:55:00 by rakman           ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c) {
-    write(1, &c, 1);
-}
+void	ft_putchar(char c) { write(1, &c, 1); }
 
-void ft_print_combn(int n)
-{
+void	ft_print_combn(int n) {
+  int i;
+  char digits[10];
 
-   int i;
-   char digits[10];
-
-    while (digits[0] < 10 - n)
-	{
-        i = n - 1;
-        while (digits[i] == i + 10 - n)
-		{
-            i--;
-        }
-        digits[i]++;
-        while (i++ < n)
-		{
-            digits[i] = digits[i - 1] + 1;
-        }
-        i = 0;
-        while (i < n)
-		{
-            ft_putchar(digits[i] + '0');
-            i++;
-        }
-        if (digits[0] != 10 - n)
-		{
-            ft_putchar(',');
-            ft_putchar(' ');
-        }
+  while (digits[0] < 10 - n) {
+    i = n - 1;
+    while (digits[i] == i + 10 - n)
+      i--;
+    digits[i]++;
+    while (i++ < n)
+      digits[i] = digits[i - 1] + 1;
+    i = 0;
+    while (i < n)
+      ft_putchar(digits[i++] + '0');
+    if (digits[0] != 10 - n) {
+      ft_putchar(',');
+      ft_putchar(' ');
     }
-}
-int main ()
-{
-	ft_print_combn(5);
+  }
 }
--- Piscine/C05/ex08/ft_the_ten_.c
+++ Piscine/C05/ex08/ft_the_ten_.c
@@ -1,38 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_the_ten_.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rakman <rakman@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 18:35:08 by rakman            #+#    #+#             */
+/*   Updated: 2024/03/17 19:43:47 by rakman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int board[10];
+int	board[10];
 
-int is_attack(int i, int j) {
-    int k, l;
-    for(k = 0; k < i; k++) {
-        if(j == board[k] || (abs(board[k] - j) == abs(i - k))) {
-            return 1;
-        }
-    }
-    return 0;
+int	is_attack(int i, int j)
+{
+	int k, l;
+	for (k = 0; k < i; k++)
+	{
+		if (j == board[k] || (abs(board[k] - j) == abs(i - k)))
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
 
-int N_queen(int n) {
-    int i, j;
-    static int count;
-    for(i = 0; i < 10; i++) {
-        if(!is_attack(n, i)) {
-            board[n] = i;
-            if(n == 9) {
-                for(j = 0; j < 10; j++) {
-                    char c = board[j] + '0';
-                    write(1, &c, 1);
-                }
-                write(1, "\n", 1);
-                count++;
-            } else {
-                N_queen(n + 1);
-            }
-        }
-    }
-    return count;
+int	N_queen(int n)
+{
+	static int	count;
+	char		c;
+
+	int i, j;
+	for (i = 0; i < 10; i++)
+	{
+		if (!is_attack(n, i))
+		{
+			board[n] = i;
+			if (n == 9)
+			{
+				for (j = 0; j < 10; j++)
+				{
+					c = board[j] + '0';
+					write(1, &c, 1);
+				}
+				write(1, "\n", 1);
+				count++;
+			}
+			else
+			{
+				N_queen(n + 1);
+			}
+		}
+	}
+	return (count);
 }
 
-int ft_ten_queens_puzzle(void) {
-    return N_queen(0);
+int	ft_ten_queens_puzzle(void)
+{
+	return (N_queen(0));
 }
