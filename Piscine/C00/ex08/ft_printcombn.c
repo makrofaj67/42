61fdbefbf91225337f40ba17e5d0f7d96baacf61
makrofaj67 <dr.ridvanakman@gmail.com>
2024-03-17 20:35:12 +0300

    fdaa

--- Piscine/C00/ex08/ft_printcombn.c
+++ Piscine/C00/ex08/ft_printcombn.c
@@ -6,31 +6,66 @@
 /*   By: rakman <rakman@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 21:06:19 by rakman            #+#    #+#             */
-/*   Updated: 2024/03/17 19:55:00 by rakman           ###   ########.fr       */
+/*   Updated: 2024/03/17 20:27:51 by rakman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c) { write(1, &c, 1); }
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
-void	ft_print_combn(int n) {
-  int i;
-  char digits[10];
+void	print_digits(char *digits, int n)
+{
+	int	i;
 
-  while (digits[0] < 10 - n) {
-    i = n - 1;
-    while (digits[i] == i + 10 - n)
-      i--;
-    digits[i]++;
-    while (i++ < n)
-      digits[i] = digits[i - 1] + 1;
-    i = 0;
-    while (i < n)
-      ft_putchar(digits[i++] + '0');
-    if (digits[0] != 10 - n) {
-      ft_putchar(',');
-      ft_putchar(' ');
-    }
-  }
+	i = 0;
+	while (i < n)
+	{
+		ft_putchar(digits[i] + 48);
+		i++;
+	}
+}
+
+void	increment_digits(char *digits, int n)
+{
+	int	i;
+
+	i = n - 1;
+	while (i >= 0 && digits[i] == 10 + i - n)
+	{
+		i--;
+	}
+	if (i >= 0)
+	{
+		digits[i]++;
+		while (i < n - 1)
+		{
+			digits[i + 1] = digits[i] + 1;
+			i++;
+		}
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	char	digits[10];
+
+	while (digits[0] < 10 - n)
+	{
+		print_digits(digits, n);
+		if (digits[0] != 10 - n)
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
+		increment_digits(digits, n);
+	}
+}
+
+int	main(void)
+{
+	ft_print_combn(1);
 }
